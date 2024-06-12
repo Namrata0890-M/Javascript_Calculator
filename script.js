5af219f39dbd624e9130277be29d617ef571d9b1
Namrata <130839490+Namrata0890-M@users.noreply.github.com>
2024-06-12 14:43:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function add (a,b) {
+    return a+b;
+}
+
+function subtract (a,b) {
+    return a-b;
+}
+
+function multilpy (a,b) {
+    return a*b;    
+}
+
+function divide (a,b) {
+    if(b==0) {
+        return 'Error ! Division by zero id undefined';
+    } else {
+    return a/b;
+    }
+}
+
